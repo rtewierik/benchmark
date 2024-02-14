d6f8fb1138824a234dcbb6fa9d8621b83f786fe6
Ruben te Wierik <ruben.wierik@vista.com>
2024-02-14 01:33:38 +0100

    First steps to add Redis deployment scripts.

---
+++ driver-redis/deploy-low-cost/provision-redis-aws.tf
@@ -0,0 +1,134 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/redis_aws.pub
+DESCRIPTION
+}
+
+resource "random_id" "hash" {
+  byte_length = 8
+}
+
+variable "key_name" {
+  default     = "benchmark-key-redis"
+  description = "Desired name of AWS key pair"
+}
+
+variable "region" {}
+
+variable "ami" {}
+
+variable "az" {}
+
+variable "instance_types" {
+  type = map(string)
+}
+
+variable "num_instances" {
+  type = map(string)
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "benchmark_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Benchmark-VPC-Redis-${random_id.hash.hex}"
+  }
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.benchmark_vpc.id
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.benchmark_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "benchmark_subnet" {
+  vpc_id                  = aws_vpc.benchmark_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.az
+}
+
+resource "aws_security_group" "benchmark_security_group" {
+  name   = "terraform-redis-${random_id.hash.hex}"
+  vpc_id = aws_vpc.benchmark_vpc.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All ports open within the VPC
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Benchmark-Security-Group-Redis-${random_id.hash.hex}"
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}-${random_id.hash.hex}"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-benchmark-cluster"
+  engine               = "redis"
+  node_type            = "cache.m5.large"
+  num_cache_nodes      = 3
+  parameter_group_name = "default.redis6.x"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "redis-benchmark-subnet-group"
+  subnet_ids = [aws_subnet.benchmark_subnet.id]
+}
+
+resource "aws_spot_instance_request" "client" {
+  ami                    = var.ami
+  instance_type          = var.instance_types["client"]
+  key_name               = aws_key_pair.auth.id
+  subnet_id              = aws_subnet.benchmark_subnet.id
+  vpc_security_group_ids = [aws_security_group.benchmark_security_group.id]
+  availability_zone      = "us-west-2a"
+  spot_type              = "one-time"
+  wait_for_fulfillment   = true
+  count                  = var.num_instances["client"]
+
+  tags = {
+    Name = "redis_client_${count.index}"
+  }
+}
+
+output "client_ssh_host" {
+  value = aws_spot_instance_request.client[0].public_ip
+}
