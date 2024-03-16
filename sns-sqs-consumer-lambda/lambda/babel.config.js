4db614205450cd526b4ccc819706027b3d9c0935
Ruben te Wierik <ruben.wierik@vista.com>
2024-03-16 15:51:57 +0100

    Remove new Lambda TS/CDK project.

--- sns-sqs-consumer-lambda/lambda/babel.config.js
+++
@@ -1,6 +0,0 @@
-﻿module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript'
-  ]
-}
--- sns-sqs-consumer-lambda/lambda/src/model/sqsBatchEvent.ts
+++
@@ -1,7 +0,0 @@
-import SqsEventRecord from './sqsEventRecord'
-
-interface SqsBatchEvent {
-  Records: SqsEventRecord[]
-}
-
-export default SqsBatchEvent
