83e3220074e3d4830cc0e0826cd0ceaa2c231a2a
Ruben te Wierik <ruben.wierik@vista.com>
2024-04-07 15:56:29 +0200

    Invert dependency of driver-api on common.

--- common/src/test/java/io/openmessaging/benchmark/common/monitoring/CountersStatsTest.java
+++ common/src/test/java/io/openmessaging/benchmark/common/monitoring/CountersStatsTest.java
@@ -11,11 +11,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.openmessaging.benchmark.driver.monitoring;
+package io.openmessaging.benchmark.common.monitoring;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.openmessaging.benchmark.common.monitoring.CountersStats;
 import org.junit.jupiter.api.Test;
 
 class CountersStatsTest {
