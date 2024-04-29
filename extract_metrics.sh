f8d59c2a11be0b88725026551b9dd67f3bd5b466
Ruben te Wierik <ruben.wierik@vista.com>
2024-04-29 21:56:03 +0200

    Fixes of the extraction script.

--- extract_metrics.sh
+++ extract_metrics.sh
@@ -18,76 +18,78 @@ disk_util=()
 for info in $(echo "${instance_info}" | jq -c '.[]'); do
   instance_id=$(echo "${info}" | jq -r '.[].InstanceId')
   private_ip=$(echo "${info}" | jq -r '.[].PrivateIpAddress')
-  echo $instance_id $private_ip
+  if [ "$private_ip" != "null" ] && [ -n "$private_ip" ]; then
+    echo $instance_id $private_ip
 
-  # Construct private hostname from private IP address
-  private_hostname="ip-${private_ip//./-}.eu-west-1.compute.internal"
-
-  # Retrieve metric data for the instance
-  output=$(aws cloudwatch get-metric-data \
-    --start-time "$(date -u -v-2H)" \
-    --end-time "$(date -u)" \
-    --metric-data-queries '[
-      {
-        "Id": "aaaa",
-        "MetricStat": {
-          "Metric": {
-            "Namespace": "AWS/EC2",
-            "MetricName": "CPUUtilization",
-            "Dimensions": [
-              {
-                "Name": "InstanceId",
-                "Value": "'"$instance_id"'"
-              }
-            ]
-          },
-          "Period": 10,
-          "Stat": "Maximum"
+    # Retrieve metric data for the instance
+    output=$(aws cloudwatch get-metric-data \
+      --start-time "$(date -u -v-2H)" \
+      --end-time "$(date -u)" \
+      --metric-data-queries '[
+        {
+          "Id": "aaaa",
+          "MetricStat": {
+            "Metric": {
+              "Namespace": "AWS/EC2",
+              "MetricName": "CPUUtilization",
+              "Dimensions": [
+                {
+                  "Name": "InstanceId",
+                  "Value": "'"$instance_id"'"
+                }
+              ]
+            },
+            "Period": 10,
+            "Stat": "Maximum"
+          }
+        },
+        {
+          "Id": "mem_available_percent",
+          "MetricStat": {
+            "Metric": {
+              "Namespace": "CWAgent",
+              "MetricName": "mem_available_percent",
+              "Dimensions": [
+                {
+                  "Name": "InstanceId",
+                  "Value": "'"$instance_id"'"
+                }
+              ]
+            },
+            "Period": 10,
+            "Stat": "Average"
+          }
+        },
+        {
+          "Id": "disk_used_percent",
+          "MetricStat": {
+            "Metric": {
+              "Namespace": "CWAgent",
+              "MetricName": "disk_used_percent",
+              "Dimensions": [
+                {
+                  "Name": "InstanceId",
+                  "Value": "'"$instance_id"'"
+                }
+              ]
+            },
+            "Period": 10,
+            "Stat": "Average"
+          }
         }
-      },
-      {
-        "Id": "mem_available_percent",
-        "MetricStat": {
-          "Metric": {
-            "Namespace": "CWAgent",
-            "MetricName": "mem_available_percent",
-            "Dimensions": [
-              {
-                "Name": "host",
-                "Value": "'"$private_hostname"'"
-              }
-            ]
-          },
-          "Period": 10,
-          "Stat": "Average"
-        }
-      },
-      {
-        "Id": "disk_used_percent",
-        "MetricStat": {
-          "Metric": {
-            "Namespace": "CWAgent",
-            "MetricName": "disk_used_percent",
-            "Dimensions": [
-              {
-                "Name": "host",
-                "Value": "'"$private_hostname"'"
-              }
-            ]
-          },
-          "Period": 10,
-          "Stat": "Average"
-        }
-      }
-    ]' \
-    --output json)
-  cpu_util_item=$(echo "${output}" | jq -r '.MetricDataResults[0].Values')
-  mem_util_item=$(echo "${output}" | jq -r '.MetricDataResults[1].Values')
-  disk_util_item=$(echo "${output}" | jq -r '.MetricDataResults[2].Values')
-  cpu_util+=",${cpu_util_item}"
-  mem_util+=",${mem_util_item}"
-  disk_util+=",${disk_util_item}"
+      ]' \
+      --output json)
+    cpu_util_item=$(echo "${output}" | jq -r '.MetricDataResults[0].Values')
+    mem_util_item=$(echo "${output}" | jq -r '.MetricDataResults[1].Values')
+    disk_util_item=$(echo "${output}" | jq -r '.MetricDataResults[2].Values')
+    cpu_util+=",${cpu_util_item}"
+    mem_util+=",${mem_util_item}"
+    disk_util+=",${disk_util_item}"
+  fi
 done
+echo "CPU Utilization"
 echo "${cpu_util:1}"
+echo "Memory Utilization"
 echo "${mem_util:1}"
+echo "Disk Utilization"
 echo "${disk_util:1}"
