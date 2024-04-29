47c2eb4e412384d87efb17a83d8de58ade10f6da
Ruben te Wierik <ruben.wierik@vista.com>
2024-04-29 14:39:26 +0200

    Add default tags for all infrastructure created for benchmarks.

--- driver-pulsar/deploy-low-cost/providers.tf
+++ driver-pulsar/deploy-low-cost/providers.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "${var.region}"
   allowed_account_ids = ["533267013102", "138945776678"]
+  default_tags {
+    tags = {
+      Application = var.app_name
+      Owner       = "Ruben te Wierik"
+      Contact     = "rtewierik@uoc.edu"
+    }
+  }
 }
 
 provider "random" {}
